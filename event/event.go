0bb6e8a34c0938438d291900d42d0d5306ac8c5e
Arthur <arthur.cavalcante.puc@gmail.com>
2022-09-21 19:30:09 -0300

    wip: job and cronjob added to tsuru database

--- event/event.go
+++ event/event.go
@@ -93,6 +93,7 @@ var (
 
 	TargetTypeGlobal          = TargetType("global")
 	TargetTypeApp             = TargetType("app")
+	TargetTypeJob             = TargetType("job")
 	TargetTypeNode            = TargetType("node")
 	TargetTypeContainer       = TargetType("container")
 	TargetTypePool            = TargetType("pool")
